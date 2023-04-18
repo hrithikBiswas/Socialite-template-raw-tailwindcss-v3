4deae6e7314209ae594cead8788fc8a482bafdc6
Hrithik Biswas <hrithikpersonalid@gmail.com>
2023-04-19 01:26:10 +0600

    featured dark mode

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,22 @@
 import '../css/styles.css';
+
+/**
+ *  ENABLE DARK MODE
+ */
+const body = document.querySelector('body');
+const theme = document.querySelectorAll('.theme');
+const darkTheme = document.getElementById('dark-theme');
+const lightTheme = document.getElementById('light-theme');
+
+for (let i = 0; i < theme.length; i++) {
+    theme[i].addEventListener('click', () => {
+        body.classList.toggle('dark');
+        if (darkTheme.classList.contains('hidden')) {
+            lightTheme.classList.add('hidden');
+            darkTheme.classList.remove('hidden');
+        } else {
+            lightTheme.classList.remove('hidden');
+            darkTheme.classList.add('hidden');
+        }
+    });
+}
