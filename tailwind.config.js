232320eea651bb77e3f0cd274856a3d22cf722df
Hrithik Biswas <hrithikpersonalid@gmail.com>
2023-04-22 18:30:42 +0600

    completed main content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/*.html', './src/**/*.js'],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            colors: {
-                'dark-foreground': '#0e1726',
-                'dark-background': '#060818',
-            },
-        },
+  content: ["./src/*.html", "./src/**/*.js"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      screens: {
+        xs: "400px",
+      },
+      colors: {
+        "dark-foreground": "#0e1726",
+        "dark-background": "#060818",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
