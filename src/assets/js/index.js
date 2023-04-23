086ecf7fa8b2957b7105109b6317c981c772d2df
Hrithik Biswas <hrithikpersonalid@gmail.com>
2023-04-23 19:02:55 +0600

    post modal created

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -11,14 +11,14 @@ sidebarBtn.addEventListener('click', () => {
     showSidebar.classList.add('left-0');
     aside.classList.add('sidebar-dropshadow');
 });
-// WHEN THEN USER CLICKS ANYWHERE OUTSIDE OF THE SIDEBAR NAVIGATION, CLOSE IT
-window.onclick = function (event) {
-    if (event.target == aside) {
-        showSidebar.classList.remove('left-0');
-        showSidebar.classList.add('-left-72');
-        aside.classList.remove('sidebar-dropshadow');
-    }
-};
+// // WHEN THEN USER CLICKS ANYWHERE OUTSIDE OF THE SIDEBAR NAVIGATION, CLOSE IT
+// window.onclick = function (event) {
+//     if (event.target == aside) {
+//         showSidebar.classList.remove('left-0');
+//         showSidebar.classList.add('-left-72');
+//         aside.classList.remove('sidebar-dropshadow');
+//     }
+// };
 
 /**
  *  SHOW & HIDE SEARCH MODAL DEPEND ON FOCUSIN OR FOCUSOUT EVENTS
@@ -33,9 +33,42 @@ searchModal.addEventListener('focusout', () => {
 });
 
 /**
+ *  SHOW POST MODAL
+ */
+const postModalInput = document.getElementById('post-modal-input');
+const postModalShow = document.getElementById('post-modal-show');
+const exitModal = document.getElementById('exit-post-modal');
+const html = document.getElementsByTagName('html')[0];
+postModalInput.addEventListener('focus', () => {
+    postModalShow.classList.remove('invisible', 'opacity-0');
+    postModalShow.classList.add('visible', 'opacity-100');
+    html.classList.add('overflow-hidden');
+});
+exitModal.addEventListener('click', () => {
+    postModalShow.classList.remove('visible', 'opacity-100');
+    postModalShow.classList.add('invisible', 'opacity-0');
+    html.classList.remove('overflow-hidden');
+});
+/**
+ *WHEN THEN USER CLICKS ANYWHERE OUTSIDE OF SIDEBAR NAVIGATION OR MODAL, CLOSE IT
+ */
+window.onclick = function (event) {
+    if (event.target == aside) {
+        showSidebar.classList.remove('left-0');
+        showSidebar.classList.add('-left-72');
+        aside.classList.remove('sidebar-dropshadow');
+    }
+    // invisible post modal
+    if (event.target == postModalShow) {
+        postModalShow.classList.remove('visible', 'opacity-100');
+        postModalShow.classList.add('invisible', 'opacity-0');
+        html.classList.remove('overflow-hidden');
+    }
+};
+
+/**
  *  ENABLE DARK MODE
  */
-
 const theme = document.querySelectorAll('.theme');
 const darkTheme = document.getElementById('dark-theme');
 const lightTheme = document.getElementById('light-theme');
